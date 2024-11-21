a8a7b229f04df6d30c652cc9767ac46a94c40adc
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-11-21 11:01:06 +0900

    chore: 코드정리

--- src/components/Interaction.jsx
+++ src/components/Interaction.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Interaction = ({ children, onClick }) => {
   return <Item onClick={onClick}>{children}</Item>;
@@ -9,11 +9,10 @@ const Item = styled.div`
   display: flex;
   align-items: center;
   color: #777;
-
   font-size: 14px;
   cursor: pointer;
   gap: 10px;
   position: relative;
 `;
 
-export default Interaction
+export default Interaction;
